b39edebc084bb30f28de1aa6e10dd5e97fc8df99
chenwen <chenwen@mogujie.com>
2017-02-20 21:03:09 +0800

    fix

--- app/src/main/java/com/test/chenwenping/androidcanvasandpaint/CanvasBitmap.java
+++ app/src/main/java/com/test/chenwenping/androidcanvasandpaint/CanvasBitmap.java
@@ -12,12 +12,13 @@ import android.graphics.PorterDuffXfermode;
  */
 
 public class CanvasBitmap {
+    //http://blog.csdn.net/yixinyouni1314/article/details/7774164
 
     public static Bitmap CreateCricleImage(Bitmap image, int min) {
 
         Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setFilterBitmap(true);
+        paint.setAntiAlias(true);//抗锯齿
+        paint.setFilterBitmap(true);//对位图进行滤波处理
 
         Bitmap target = Bitmap.createBitmap(min, min, image.getConfig());
 
@@ -27,6 +28,9 @@ public class CanvasBitmap {
 
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
 
+        //你不可能控制每个窗口的绘制的时候，您就需要用下面的方法来处理——对整个Canvas进行处理。 1）在您的构造函数中，创建一个Paint滤波器。
+        //PaintFlagsDrawFilter
+        //当你在画图的时候，如果是View则在onDraw当中，如果是ViewGroup则在dispatchDraw中调用如下函数。
         canvas.setDrawFilter(new PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG));
 
         canvas.drawBitmap(image, 0, 0, paint);
